7f66d04c5491818253179dba8aca8422e1eebd66
Graham <gmcallister@gatech.edu>
2016-01-30 14:17:13 -0500

    new connection

--- src/Runner.java
+++ src/Runner.java
@@ -4,15 +4,15 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		//Start connection to market
-		Connection connect = new Connection();
-		connect.init();
-		//Run for entire market period
-		while (true) {
-			//Scanner for pushing commands
-			Scanner commands = new Scanner(System.in);
-			//Update securities
-			//Check for possible trades
-			update()
+		// Connection connect = new Connection();
+		// connect.init();
+		// //Run for entire market period
+		// while (true) {
+		// 	//Scanner for pushing commands
+		// 	Scanner commands = new Scanner(System.in);
+		// 	//Update securities
+		// 	//Check for possible trades
+		// 	update()
 		}
 	}
 }
